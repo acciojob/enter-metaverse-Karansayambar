48a37490de8248103c78f25fe599d8e227782d8c
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2023-12-26 07:49:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const para = document.getElementById("para");
+const click = document.getElementById("click");
+
+function buttonClick(){
+	para.innerText="Enter the Metaverse"
+	para.style.fontWeight = "bold";
+	para.style.fontSize = "20px"
+}

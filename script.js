ac795417c866211a6a8fe4c52703b39505b75661
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2023-12-26 15:10:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 //your JS code here. If required.
 const para = document.getElementById('status');
-
 function buttonClick(){
 	const head = document.createElement("h1");
 	para.innerText="Entered Metaverse";
 	head.innerText = para.innerText;
-	para.parentNode.replaceChild(head,para);
 }
